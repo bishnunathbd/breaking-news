b7a449d494c9d622ac926c024c302a9ac9204c3c
bishnunathbd <bishnunath774@gmail.com>
2021-04-07 21:28:47 +0600

    36-getting started with react-bootstrap

---
+++ src/components/TopHeadline/TopHeadline.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import News from '../News/News';
+
+const TopHeadline = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=39797bf826274bfeb561df80dd97059a';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setArticles(data.articles))
+  }, [])
+
+  return (
+    <div>
+      <h2>Top Headline: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
+    </div>
+  );
+};
+
+export default TopHeadline;
